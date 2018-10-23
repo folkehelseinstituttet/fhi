f554cc0289fdad0972b0ac5440779d309e5ef9a8
Beatriz Valcarcel <GryMaryson.Groneng@fhi.no>
2018-10-23 14:46:20 +0200

    template for SP pdf - mage og luft

---
+++ R/sykdompulsepdf_document.R
@@ -0,0 +1,34 @@
+#' hi
+#' @param ... test
+#' @importFrom rmarkdown pandoc_available pdf_document
+#' @export sykdompulsepdf_document_mage
+sykdompulsepdf_document_mage <- function(...) {
+
+  rmarkdown::pandoc_available('1.9', TRUE)
+
+  template <- system.file("rmarkdown", "templates", "sykdomspulspdf_mage", "resources", "template.latex",
+                          package = "fhi")
+
+  base <- rmarkdown::pdf_document(... , template=template, latex_engine = "xelatex")
+  base$inherits <- "pdf_document"
+
+  base
+}
+
+
+#' hi
+#' @param ... test
+#' @importFrom rmarkdown pandoc_available pdf_document
+#' @export sykdompulsepdf_document_luft
+sykdompulsepdf_document_luft <- function(...) {
+  
+  rmarkdown::pandoc_available('1.9', TRUE)
+  
+  template <- system.file("rmarkdown", "templates", "sykdomspulspdf_luft", "resources", "template.latex",
+                          package = "fhi")
+  
+  base <- rmarkdown::pdf_document(... , template=template, latex_engine = "xelatex")
+  base$inherits <- "pdf_document"
+  
+  base
+}
