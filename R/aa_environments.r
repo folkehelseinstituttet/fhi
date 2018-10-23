cd2c4fe82916bd6b3f6b94ab4759eccbc91c538d
Richard White <r@rwhite.no>
2018-10-23 11:39:24 +0300

    RenderExternally+dashboardopinionated

--- R/aa_environments.r
+++ R/aa_environments.r
@@ -1,6 +1,11 @@
 #' PROJ
 #' @export PROJ
 PROJ <- new.env(parent = emptyenv())
+PROJ$COMPUTER_NAME <- "x"
+PROJ$PRODUCTION_NAME <- "smhb"
+PROJ$IS_PRODUCTION <- FALSE
+PROJ$IS_TESTING <- FALSE
+
 
 #' countyToMunicip
 #' @docType data
