e7a4a9f88055c2114d16c90e28be4c73d7dc5d5f
Richard White <r@rwhite.no>
2018-10-25 11:09:43 +0300

    pkgdown

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# fhi 2018.10.12
+
+## Migration
+
+Migrating from [raubreywhite/fhi](https://www.github.com/raubreywhite/fhi/) to [folkehelseinstituttet/fhi](https://www.github.com/folkehelseinstituttet/fhi/)
---
+++ R/normomo_document.R
@@ -0,0 +1,47 @@
+#' hi
+#' @param ... test
+#' @importFrom rmarkdown pandoc_available pdf_document
+#' @export normomo_document
+normomo_document <- function(...) {
+
+  rmarkdown::pandoc_available('1.9', TRUE)
+
+  template <- system.file("rmarkdown", "templates", "normomo", "resources", "template.latex",
+                                      package = "fhi")
+
+  base <- rmarkdown::pdf_document(... , template=template, latex_engine = "xelatex")
+  base$inherits <- "pdf_document"
+
+  base
+}
+
+#' hi
+#' @param output_dir a
+#' @export normomo_resources_copy
+normomo_resources_copy <- function(output_dir){
+  dir <- system.file("rmarkdown", "templates", "normomo", "skeleton",
+                     package = "fhi")
+  files <- list.files(dir)
+  files <- files[tolower(files)!="skeleton.rmd"]
+
+  for(f in files){
+    file.copy(
+      from=file.path(dir,f),
+      to=file.path(output_dir,f))
+  }
+}
+
+
+#' hi
+#' @param output_dir a
+#' @export normomo_resources_remove
+normomo_resources_remove <- function(output_dir){
+  dir <- system.file("rmarkdown", "templates", "normomo", "skeleton",
+                     package = "fhi")
+  files <- list.files(dir)
+  files <- files[tolower(files)!="skeleton.rmd"]
+
+  for(f in files){
+    file.remove(file.path(output_dir,f))
+  }
+}
