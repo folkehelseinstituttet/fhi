55f9100e1aa0ba495808bf731b56e91c33a5b107
Richard White <r@rwhite.no>
2018-10-26 14:14:12 +0300

    Documentation

--- R/aa_environments.r
+++ R/aa_environments.r
@@ -5,7 +5,11 @@ PROJ$COMPUTER_NAME <- "x"
 PROJ$PRODUCTION_NAME <- "smhb"
 PROJ$IS_PRODUCTION <- FALSE
 PROJ$IS_DEV <- FALSE
-PROJ$IS_INITIALIZED <- FALSE
+PROJ$IS_INITIALISED <- FALSE
+
+#' SYSCALLS
+#' @export SYSCALLS
+SYSCALLS <- new.env(parent = emptyenv())
 
 
 #' countyToMunicip
