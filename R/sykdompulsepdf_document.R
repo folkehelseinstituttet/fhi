c3b925b48ea7491a4f87e7cdea556658dcf51acc
Beatriz Valcarcel <GryMaryson.Groneng@fhi.no>
2018-10-30 14:23:26 +0100

    add copy and remove functions

--- R/sykdompulsepdf_document.R
+++ R/sykdompulsepdf_document.R
@@ -1,12 +1,12 @@
 #' hi
 #' @param ... test
 #' @importFrom rmarkdown pandoc_available pdf_document
-#' @export sykdompulsepdf_document_mage
-sykdompulsepdf_document_mage <- function(...) {
+#' @export sykdompuls_mage_document
+sykdompuls_mage_document <- function(...) {
 
   rmarkdown::pandoc_available('1.9', TRUE)
 
-  template <- system.file("rmarkdown", "templates", "sykdomspulspdf_mage", "resources", "template.latex",
+  template <- system.file("rmarkdown", "templates", "sykdompuls_mage_document", "resources", "template.latex",
                           package = "fhi")
 
   base <- rmarkdown::pdf_document(... , template=template, latex_engine = "xelatex")
@@ -19,16 +19,45 @@ sykdompulsepdf_document_mage <- function(...) {
 #' hi
 #' @param ... test
 #' @importFrom rmarkdown pandoc_available pdf_document
-#' @export sykdompulsepdf_document_luft
-sykdompulsepdf_document_luft <- function(...) {
-  
+#' @export sykdompuls_luft_document
+sykdompuls_luft_document <- function(...) {
+
   rmarkdown::pandoc_available('1.9', TRUE)
-  
-  template <- system.file("rmarkdown", "templates", "sykdomspulspdf_luft", "resources", "template.latex",
+
+  template <- system.file("rmarkdown", "templates", "sykdompuls_luft_document", "resources", "template.latex",
                           package = "fhi")
-  
+
   base <- rmarkdown::pdf_document(... , template=template, latex_engine = "xelatex")
   base$inherits <- "pdf_document"
-  
+
   base
+}
+
+
+#' hi
+#' @param output_dir a
+#' @export sykdompulspdf_resources_copy
+sykdompulspdf_resources_copy <- function(output_dir){
+  dir <- system.file("rmarkdown", "templates", "sykdompuls_luft_document", "skeleton",
+                     package = "fhi")
+  files <- list.files(dir,pattern="^_skeleton")
+  for(f in files){
+    file.copy(
+      from=file.path(dir,f),
+      to=file.path(output_dir,f))
+  }
+}
+
+
+#' hi
+#' @param output_dir a
+#' @export sykdompulspdf_resources_remove
+sykdompulspdf_resources_remove <- function(output_dir){
+  dir <- system.file("rmarkdown", "templates", "sykdompuls_luft_document", "skeleton",
+                     package = "fhi")
+  files <- list.files(dir,pattern="^_skeleton")
+
+  for(f in files){
+    file.remove(file.path(output_dir,f))
+  }
 }
