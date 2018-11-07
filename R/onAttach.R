6f15f6189ab404432e9c46a1cf1874c6ff6c163f
Richard White <w@rwhite.no>
2018-11-07 23:03:33 +0100

    versioning

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: FHI")
-  packageStartupMessage("Version 2018.11.06")
+  packageStartupMessage("Version 2018.11.07 @ 22:02")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
