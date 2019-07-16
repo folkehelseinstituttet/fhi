d4a63a82ce9b242d09d7ae5454efe85614477bf3
Richard White <w@rwhite.no>
2019-07-16 12:42:18 +0300

    Update file_stable.R

--- R/file_stable.R
+++ R/file_stable.R
@@ -1,3 +1,11 @@
+file_open <- function(file = tempfile()){
+  suppressWarnings(
+    "try-error" %in% class(
+      try(file(file, open = "w"), silent = TRUE)
+    )
+  )
+}
+
 #' Is the file stable (size not increasing/decreasing)?
 #'
 #' @param file The file of interest
@@ -8,7 +16,11 @@ file_stable <- function(file = tempfile(), delay = 10) {
     size1 <- file.info(file)$size[1]
     Sys.sleep(delay)
     size2 <- file.info(file)$size[1]
-    return(size1 == size2)
+    if(size1 == size2){
+      if(!file_open(file)){
+        return(TRUE)
+      }
+    }
   }
   return(FALSE)
 }
