7f2fd349d9b6c75859e2e831593e1d95ecaf2ec6
Richard White <w@rwhite.no>
2020-08-11 10:59:41 +0200

    downgrade cpp11 to v0.1

--- travis/make.sh
+++ travis/make.sh
@@ -6,6 +6,9 @@ sed -i "s/^Version: .*$/Version: $DATE/" DESCRIPTION
 head DESCRIPTION
 
 sed -i "s/packageStartupMessage(\"Version.*$/packageStartupMessage(\"Version $DATETIME\")/" R/onAttach.R
+
+Rscript -e 'devtools::install_version("cpp11", version = "0.1", repos = "http://cran.us.r-project.org")'
+
 ## Other options:
 ## Only add if the commit is tagged: so something like:
 #if [ $TRAVIS_TAG ] ; then
